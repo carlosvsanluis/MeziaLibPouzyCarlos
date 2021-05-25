66f1f0bd7dd86415f46f7148699232dc2113a087
carlosvsanluis <carlosvsanluis@gmail.com>
2021-05-26 01:03:28 +0200

    Supprimer Les Livres

--- src/mezialibpouzycarlos/PouzyApp.java
+++ src/mezialibpouzycarlos/PouzyApp.java
@@ -119,6 +119,8 @@ public class PouzyApp extends javax.swing.JFrame {
         exitButton = new javax.swing.JButton();
         clearButton = new javax.swing.JButton();
         insertAvailable = new javax.swing.JTextField();
+        jButton1 = new javax.swing.JButton();
+        showBooks = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -130,6 +132,11 @@ public class PouzyApp extends javax.swing.JFrame {
         });
 
         deleteBookButton.setText("Delete Book");
+        deleteBookButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                deleteBookButtonActionPerformed(evt);
+            }
+        });
 
         updateBookButton.setText("UpdateBook");
         updateBookButton.addActionListener(new java.awt.event.ActionListener() {
@@ -162,7 +169,7 @@ public class PouzyApp extends javax.swing.JFrame {
             }
         });
         jScrollPane1.setViewportView(jTable1);
-        jTable1.getColumnModel().getSelectionModel().setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
+        jTable1.getColumnModel().getSelectionModel().setSelectionMode(javax.swing.ListSelectionModel.SINGLE_INTERVAL_SELECTION);
 
         insertAuthor.setText("Author");
 
@@ -203,50 +210,69 @@ public class PouzyApp extends javax.swing.JFrame {
             }
         });
 
+        jButton1.setText("TODO Show Users");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        showBooks.setText("Afficher Les Livres");
+        showBooks.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                showBooksActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                    .addComponent(clearButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(exitButton, javax.swing.GroupLayout.DEFAULT_SIZE, 91, Short.MAX_VALUE))
-                .addGap(241, 241, 241))
             .addGroup(layout.createSequentialGroup()
                 .addGap(49, 49, 49)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(insertTitle, javax.swing.GroupLayout.PREFERRED_SIZE, 165, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
-                        .addComponent(insertAuthor, javax.swing.GroupLayout.PREFERRED_SIZE, 165, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
-                        .addComponent(insertLanguage, javax.swing.GroupLayout.PREFERRED_SIZE, 165, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
-                        .addComponent(insertIBSN, javax.swing.GroupLayout.PREFERRED_SIZE, 165, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
-                        .addComponent(insertAvailable, javax.swing.GroupLayout.PREFERRED_SIZE, 165, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addComponent(clearButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(exitButton, javax.swing.GroupLayout.DEFAULT_SIZE, 91, Short.MAX_VALUE))
+                        .addGap(241, 241, 241))
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(addBookButton)
-                        .addGap(115, 115, 115)
-                        .addComponent(updateBookButton)
-                        .addGap(114, 114, 114)
-                        .addComponent(deleteBookButton))
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 897, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(156, Short.MAX_VALUE))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(insertTitle, javax.swing.GroupLayout.PREFERRED_SIZE, 165, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
+                                .addComponent(insertAuthor, javax.swing.GroupLayout.PREFERRED_SIZE, 165, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
+                                .addComponent(insertLanguage, javax.swing.GroupLayout.PREFERRED_SIZE, 165, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
+                                .addComponent(insertIBSN, javax.swing.GroupLayout.PREFERRED_SIZE, 165, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
+                                .addComponent(insertAvailable, javax.swing.GroupLayout.PREFERRED_SIZE, 165, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(addBookButton)
+                                .addGap(115, 115, 115)
+                                .addComponent(updateBookButton)
+                                .addGap(114, 114, 114)
+                                .addComponent(deleteBookButton))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 897, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
+                                .addComponent(showBooks)))
+                        .addGap(17, 17, Short.MAX_VALUE))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(34, 34, 34)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 188, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(138, 138, 138)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(addBookButton)
-                            .addComponent(updateBookButton)
-                            .addComponent(deleteBookButton)))
+                        .addGap(34, 34, 34)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 188, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(99, 99, 99)
+                        .addComponent(showBooks, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addGap(40, 40, 40)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
@@ -256,10 +282,18 @@ public class PouzyApp extends javax.swing.JFrame {
                             .addComponent(insertAvailable, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(insertIBSN, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGap(18, 18, 18)
-                        .addComponent(clearButton)
-                        .addGap(111, 111, 111)
-                        .addComponent(exitButton)))
-                .addContainerGap(117, Short.MAX_VALUE))
+                        .addComponent(clearButton))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(138, 138, 138)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(addBookButton)
+                            .addComponent(updateBookButton)
+                            .addComponent(deleteBookButton))))
+                .addGap(65, 65, 65)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 56, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(exitButton))
+                .addContainerGap(84, Short.MAX_VALUE))
         );
 
         pack();
@@ -318,13 +352,9 @@ private JFrame frame;
 
     private void clearButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_clearButtonActionPerformed
         insertAuthor.setText("");
-
         insertLanguage.setText("");
-
         insertIBSN.setText("");
-
         insertTitle.setText("");
-        
         insertAvailable.setText("");
     }//GEN-LAST:event_clearButtonActionPerformed
 
@@ -337,17 +367,24 @@ private JFrame frame;
     }//GEN-LAST:event_insertAvailableActionPerformed
 
     private void updateBookButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_updateBookButtonActionPerformed
-         try
+        
+         DefaultTableModel RecordTable = (DefaultTableModel)jTable1.getModel();
+         int SelectedRows = jTable1.getSelectedRow();
+        
+        try
        {
+           id = Integer.parseInt(RecordTable.getValueAt(SelectedRows, 0).toString());
            Class.forName("com.mysql.jdbc.Driver");
            sqlConn = DriverManager.getConnection(dataConn,username,password);
-           pst = sqlConn.prepareStatement("UPDATE books set title=?, author=?, language=?, ibsn=?, available=? WHERE id=?");
+           pst = sqlConn.prepareStatement("UPDATE books set title=?, author=?, language=?, ibsn=?, available=? WHERE title=?");
            
            pst.setString(1, insertTitle.getText());
            pst.setString(2, insertAuthor.getText());
            pst.setString(3, insertLanguage.getText());
            pst.setString(4, insertIBSN.getText());
            pst.setString(5, insertAvailable.getText());
+           pst.setString(6, insertTitle.getText());
+           
            
            pst.executeUpdate();
            JOptionPane.showMessageDialog(this, "Record Updated");
@@ -368,6 +405,7 @@ private JFrame frame;
     }//GEN-LAST:event_updateBookButtonActionPerformed
 
     private void jTable1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jTable1MouseClicked
+         
         DefaultTableModel RecordTable = (DefaultTableModel)jTable1.getModel();
          int SelectedRows = jTable1.getSelectedRow();   
          
@@ -378,11 +416,62 @@ private JFrame frame;
          insertAvailable.setText(RecordTable.getValueAt(SelectedRows, 5).toString());
            
     }//GEN-LAST:event_jTable1MouseClicked
+
+    private void deleteBookButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_deleteBookButtonActionPerformed
+        
+        DefaultTableModel RecordTable = (DefaultTableModel)jTable1.getModel();
+        int SelectedRows = jTable1.getSelectedRow(); 
+        
+        try 
+        {
+            id = Integer.parseInt(RecordTable.getValueAt(SelectedRows, 0).toString());
+            
+            deleteItem = JOptionPane.showConfirmDialog(null,"Confirm if you want delete Item",
+                    "Warning", JOptionPane.YES_NO_OPTION);
+            if (deleteItem == JOptionPane.YES_OPTION) 
+            {
+                Class.forName("com.mysql.jdbc.Driver");
+                sqlConn =DriverManager.getConnection(dataConn,username,password);
+                pst = sqlConn.prepareStatement("DELETE from books where idbook =?");
+                
+                pst.setInt(1, id);
+                pst.executeUpdate();
+             JOptionPane.showMessageDialog(this,"Books Record Updates");
+             updateDB();
+             
+         insertTitle.setText("");
+         insertTitle.requestFocus();
+         insertAuthor.setText("");
+         insertLanguage.setText("");
+         insertIBSN.setText("");
+         insertAvailable.setText("");
+       
+            }
+        }
+              catch (ClassNotFoundException ex){
+            java.util.logging.Logger.getLogger(PouzyApp.class.getName()).log(java.util.logging.Level.SEVERE,
+                    null, ex);
+        }
+        catch (SQLException ex){
+            System.err.println(ex);
+        }
+    }//GEN-LAST:event_deleteBookButtonActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void showBooksActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_showBooksActionPerformed
+        updateDB();
+    }//GEN-LAST:event_showBooksActionPerformed
        
     /**
      * @param args the command line arguments
      */
     public static void main(String args[]) {
+        
+        
+      
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
@@ -410,9 +499,11 @@ private JFrame frame;
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new PouzyApp().setVisible(true);
+               
             }
         });
     }
+    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton addBookButton;
@@ -424,8 +515,10 @@ private JFrame frame;
     private javax.swing.JTextField insertIBSN;
     private javax.swing.JTextField insertLanguage;
     private javax.swing.JTextField insertTitle;
+    private javax.swing.JButton jButton1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
+    private javax.swing.JButton showBooks;
     private javax.swing.JButton updateBookButton;
     // End of variables declaration//GEN-END:variables
 
